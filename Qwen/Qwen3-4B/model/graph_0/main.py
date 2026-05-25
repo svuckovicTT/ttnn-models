6c534016a5647bc6a55dd50c9074c278dd050dc4
svuckovic <svuckovic@tenstorrent.com>
2026-05-25 08:36:34 +0000

    [prettify] post_load_weights - step 4 (attempt 0)

--- Qwen/Qwen3-4B/model/graph_0/main.py
+++ Qwen/Qwen3-4B/model/graph_0/main.py
@@ -2,7 +2,7 @@ import ttnn
 import model_pt
 import utils
 from consteval import consteval__main
-from params import load_weights_for__main
+from params import load_weights_for__main_from_state_dict
 from utils import calculate_pcc
 
 ce_cache__main = {}
@@ -25142,7 +25142,7 @@ def load_activations_for__main():
 
 def main():
     load_activations_for__main_0 = load_activations_for__main()
-    load_weights_for__main_0 = load_weights_for__main()
+    load_weights_for__main_0 = load_weights_for__main_from_state_dict()
     _main_0 = _main(load_activations_for__main_0, load_weights_for__main_0)
     return 0
 
@@ -25164,7 +25164,7 @@ def test_main():
         ),
     )
 
-    weights = load_weights_for__main()
+    weights = load_weights_for__main_from_state_dict()
     outputs = _main([input], weights)
 
     ttnn_output = ttnn.to_torch(ttnn.from_device(outputs[-1]))
