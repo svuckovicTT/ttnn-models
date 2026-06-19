176a77913fda850bd191ae9b403f5587701af832
svuckovic <svuckovic@tenstorrent.com>
2026-06-19 22:22:34 +0000

    [prettify] classify_ttnn_model - step 7 (attempt 0)

--- microsoft/resnet-50/model/graph_0/main.py
+++ microsoft/resnet-50/model/graph_0/main.py
@@ -2,9 +2,8 @@ import ttnn
 import torch
 import utils
 import model_pt
-from model_ttnn import _main
+from model_ttnn import ModelTTNN
 from utils import calculate_pcc
-from params import load_weights_for__main_from_state_dict as load_weights_for__main
 
 
 def load_activations_for__main(device):
@@ -22,9 +21,9 @@ def load_activations_for__main(device):
 
 def main():
     device = utils.open_device()
+    model = ModelTTNN(device)
     load_activations_for__main_0 = load_activations_for__main(device)
-    load_weights_for__main_0 = load_weights_for__main()
-    _main_0 = _main(device, load_activations_for__main_0, load_weights_for__main_0)
+    _main_0 = model(load_activations_for__main_0)
     utils.close_device(device)
     return 0
 
@@ -33,12 +32,12 @@ def test_main():
     exact_pcc = 0.97265625
 
     device = utils.open_device()
+    model = ModelTTNN(device)
     input_tensor = model_pt.load_input()
     ttnn_input = ttnn.from_torch(input_tensor, dtype=ttnn.DataType.BFLOAT16, layout=ttnn.Layout.ROW_MAJOR, device=device, memory_config=ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None))
     activations = [ttnn_input]
 
-    weights = load_weights_for__main()
-    outputs = _main(device, activations, weights)
+    outputs = model(activations)
 
     ttnn_output = ttnn.to_torch(ttnn.from_device(outputs[0]))
     golden_output = model_pt.run_pytorch_model()
